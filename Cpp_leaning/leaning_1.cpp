a02987bf54924b6100ba327e9f5344bb07147c47
sabina233 <2306595542@qq.com>
2024-12-25 13:36:18 +0800

    test111

--- Cpp_leaning/leaning_1.cpp
+++ Cpp_leaning/leaning_1.cpp
@@ -1,55 +1,291 @@
-// 俄罗斯方块
-#include <iostream>
-#include <vector>
-#include <cstdlib>
-#include <conio.h> // for _kbhit() and _getch()
+#include <windows.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
+// 游戏区域的宽度和高度（以方块为单位）
 const int WIDTH = 10;
 const int HEIGHT = 20;
-std::vector<std::vector<int>> board(HEIGHT, std::vector<int>(WIDTH, 0));
 
-void drawBoard() {
-    system("cls");
-    for (const auto& row : board) {
-        for (int cell : row) {
-            std::cout << (cell ? "#" : ".") << " ";
+// 方块的大小（像素）
+const int BLOCK_SIZE = 30;
+	
+// 定义方块结构体
+struct Block {
+    int x;
+    int y;
+    int shape[4][4];
+};
+
+// 不同形状的方块模板
+Block blocks[7] = {
+    {0, 0, {
+        {1, 1, 1, 1},
+        {0, 0, 0, 0},
+        {0, 0, 0, 0},
+        {0, 0, 0, 0}
+    }},
+    {0, 0, {
+        {1, 1, 0, 0},
+        {1, 1, 0, 0},
+        {0, 0, 0, 0},
+        {0, 0, 0, 0}
+    }},
+    {0, 0, {
+        {1, 1, 1, 0},
+        {0, 0, 1, 0},
+        {0, 0, 0, 0},
+        {0, 0, 0, 0}
+    }},
+    {0, 0, {
+        {1, 1, 1, 0},
+        {1, 0, 0, 0},
+        {0, 0, 0, 0},
+        {0, 0, 0, 0}
+    }},
+    {0, 0, {
+        {1, 0, 0, 0},
+        {1, 1, 1, 0},
+        {0, 0, 0, 0},
+        {0, 0, 0, 0}
+    }},
+    {0, 0, {
+        {0, 0, 1, 0},
+        {1, 1, 1, 0},
+        {0, 0, 0, 0},
+        {0, 0, 0, 0}
+    }},
+    {0, 0, {
+        {1, 1, 0, 0},
+        {0, 1, 1, 0},
+        {0, 0, 0, 0},
+        {0, 0, 0, 0}
+    }}
+};
+
+// 游戏区域的二维数组，用于记录每个位置是否有方块
+int gameBoard[HEIGHT][WIDTH] = { 0 };
+
+// 窗口句柄
+HWND hWnd;
+
+// 绘制单个方块
+void DrawBlock(HDC hdc, int x, int y, int color) {
+    HBRUSH hBrush = CreateSolidBrush(color);
+    SelectObject(hdc, hBrush);
+    Rectangle(hdc, x * BLOCK_SIZE, y * BLOCK_SIZE, (x + 1) * BLOCK_SIZE, (y + 1) * BLOCK_SIZE);
+    DeleteObject(hBrush);
+}
+
+// 绘制游戏区域和已落下的方块
+void DrawGameBoard(HDC hdc) {
+    // 绘制游戏区域边框
+    HBRUSH hBorderBrush = CreateSolidBrush(RGB(100, 100, 100));
+    SelectObject(hdc, hBorderBrush);
+    Rectangle(hdc, 0, 0, WIDTH * BLOCK_SIZE, HEIGHT * BLOCK_SIZE);
+    DeleteObject(hBorderBrush);
+
+    // 绘制已落下的方块
+    for (int y = 0; y < HEIGHT; y++) {
+        for (int x = 0; x < WIDTH; x++) {
+            if (gameBoard[y][x]) {
+                DrawBlock(hdc, x, y, RGB(255, 0, 0));
+            }
         }
-        std::cout << "\n";
     }
 }
 
-bool isValidMove(int shape, int rotation, int posX, int posY) {
-    // Simplified collision detection (assumes 1 shape for example)
-    return posX >= 0 && posX < WIDTH && posY < HEIGHT;
+// 绘制当前下落的方块
+void DrawCurrentBlock(HDC hdc, Block block) {
+    for (int y = 0; y < 4; y++) {
+        for (int x = 0; x < 4; x++) {
+            if (block.shape[y][x]) {
+                DrawBlock(hdc, block.x + x, block.y + y, RGB(0, 255, 0));
+            }
+        }
+    }
 }
 
-void placeShape(int shape, int rotation, int posX, int posY) {
-    board[posY][posX] = 1; // Simplified for placing one block
+// 初始化游戏
+void InitGame() {
+    srand(time(NULL));
+    for (int y = 0; y < HEIGHT; y++) {
+        for (int x = 0; x < WIDTH; x++) {
+            gameBoard[y][x] = 0;
+        }
+    }
 }
 
-int main() {
-    int shape = 0, rotation = 0, posX = WIDTH / 2, posY = 0;
-
-    while (true) {
-        drawBoard();
-        if (_kbhit()) {
-            char ch = _getch();
-            if (ch == 'a') posX--; // Move left
-            else if (ch == 'd') posX++; // Move right
-            else if (ch == 's') posY++; // Move down
-            else if (ch == 'q') break; // Quit
+// 检查方块是否可以移动到指定位置
+bool CanMove(Block block, int newX, int newY) {
+    for (int y = 0; y < 4; y++) {
+        for (int x = 0; x < 4; x++) {
+            if (block.shape[y][x]) {
+                int boardX = block.x + x + newX;
+                int boardY = block.y + y + newY;
+                if (boardX < 0 || boardX >= WIDTH || boardY >= HEIGHT || gameBoard[boardY][boardX]) {
+                    return false;
+                }
+            }
         }
+    }
+    return true;
+}
 
-        if (isValidMove(shape, rotation, posX, posY)) {
-            placeShape(shape, rotation, posX, posY);
-            posY++;
+// 让方块下落一步
+void MoveDown(Block& block) {
+    if (CanMove(block, 0, 1)) {
+        block.y++;
+    }
+    else {
+        // 将方块固定到游戏区域
+        for (int y = 0; y < 4; y++) {
+            for (int x = 0; x < 4; x++) {
+                if (block.shape[y][x]) {
+                    gameBoard[block.y + y][block.x + x] = 1;
+                }
+            }
         }
+    }
+}
 
-        if (posY >= HEIGHT) {
-            // Reset position for next shape
-            posX = WIDTH / 2;
-            posY = 0;
+// 生成新的方块
+Block NewBlock() {
+    Block newBlock = blocks[rand() % 7];
+    newBlock.x = WIDTH / 2 - 2;
+    newBlock.y = 0;
+    return newBlock;
+}
+
+// 清除已满的行
+int ClearLines() {
+    int linesCleared = 0;
+    for (int y = HEIGHT - 1; y >= 0; y--) {
+        bool lineFull = true;
+        for (int x = 0; x < WIDTH; x++) {
+            if (!gameBoard[y][x]) {
+                lineFull = false;
+                break;
+            }
         }
+        if (lineFull) {
+            linesCleared++;
+            for (int moveY = y; moveY > 0; moveY--) {
+                for (int x = 0; x < WIDTH; x++) {
+                    gameBoard[moveY][x] = gameBoard[moveY - 1][x];
+                }
+            }
+            for (int x = 0; x < WIDTH; x++) {
+                gameBoard[0][x] = 0;
+            }
+            y++;
+        }
+    }
+    return linesCleared;
+}
+
+// 窗口过程函数，处理窗口消息
+LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam) {
+    switch (message) {
+    case WM_PAINT:
+    {
+        PAINTSTRUCT ps;
+        HDC hdc = BeginPaint(hWnd, &ps);
+        DrawGameBoard(hdc);
+        Block currentBlock = *(Block*)GetWindowLongPtr(hWnd, GWLP_USERDATA);
+        DrawCurrentBlock(hdc, currentBlock);
+        EndPaint(hWnd, &ps);
+    }
+    break;
+    case WM_KEYDOWN:
+    {
+        Block currentBlock = *(Block*)GetWindowLongPtr(hWnd, GWLP_USERDATA);
+        switch (wParam) {
+        case VK_LEFT:
+            if (CanMove(currentBlock, -1, 0)) {
+                currentBlock.x--;
+            }
+            break;
+        case VK_RIGHT:
+            if (CanMove(currentBlock, 1, 0)) {
+                currentBlock.x++;
+            }
+            break;
+        case VK_DOWN:
+            MoveDown(currentBlock);
+            break;
+        case VK_UP:
+            // 这里可以实现方块旋转逻辑（暂未完整实现旋转，仅作示意）
+            break;
+        }
+        SetWindowLongPtr(hWnd, GWLP_USERDATA, (LONG_PTR)&currentBlock);
+        InvalidateRect(hWnd, NULL, TRUE);
+    }
+    break;
+    case WM_DESTROY:
+        PostQuitMessage(0);
+        break;
+    default:
+        return DefWindowProc(hWnd, message, wParam, lParam);
     }
     return 0;
+}
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
+    PSTR szCmdLine, int iCmdShow) {
+    // 注册窗口类
+    WNDCLASS wndclass;
+    wndclass.style = CS_HREDRAW | CS_VREDRAW;
+    wndclass.lpfnWndProc = WndProc;
+    wndclass.cbClsExtra = 0;
+    wndclass.cbWndExtra = sizeof(Block*);
+    wndclass.hInstance = hInstance;
+    wndclass.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+    wndclass.hCursor = LoadCursor(NULL, IDC_ARROW);
+    wndclass.hbrBackground = (HBRUSH)GetStockObject(WHITE_BRUSH);
+    wndclass.lpszMenuName = NULL;
+    wndclass.lpszClassName = TEXT("Tetris");
+
+    if (!RegisterClass(&wndclass)) {
+        MessageBox(NULL, TEXT("注册窗口类失败！"), TEXT("错误"), MB_OK | MB_ICONERROR);
+        return 0;
+    }
+
+    // 创建窗口
+    hWnd = CreateWindow(TEXT("Tetris"), TEXT("俄罗斯方块"),
+        WS_OVERLAPPEDWINDOW,
+        CW_USEDEFAULT, CW_USEDEFAULT,
+        WIDTH * BLOCK_SIZE + 16, HEIGHT * BLOCK_SIZE + 39,
+        NULL, NULL, hInstance, NULL);
+
+    if (!hWnd) {
+        MessageBox(NULL, TEXT("创建窗口失败！"), TEXT("错误"), MB_OK | MB_ICONERROR);
+        return 0;
+    }
+
+    ShowWindow(hWnd, iCmdShow);
+    UpdateWindow(hWnd);
+
+    // 初始化游戏
+    InitGame();
+    Block currentBlock = NewBlock();
+    SetWindowLongPtr(hWnd, GWLP_USERDATA, (LONG_PTR)&currentBlock);
+
+    // 消息循环
+    MSG msg;
+    while (GetMessage(&msg, NULL, 0, 0)) {
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
+
+        // 让方块自动下落
+        Block currentBlock = *(Block*)GetWindowLongPtr(hWnd, GWLP_USERDATA);
+        MoveDown(currentBlock);
+        SetWindowLongPtr(hWnd, GWLP_USERDATA, (LONG_PTR)&currentBlock);
+        InvalidateRect(hWnd, NULL, TRUE);
+
+        // 检查是否有行已满并清除
+        ClearLines();
+    }
+
+    return msg.wParam;
 }
--- Cpp_leaning/leaning_2.cpp
+++ Cpp_leaning/leaning_2.cpp
@@ -1,181 +1,250 @@
+#include <algorithm>
+#include <iomanip>
 #include <iostream>
-#include <windows.h>  // 用于控制控制台显示相关，比如设置光标位置等
-#include <conio.h>    // 用于获取键盘输入
-#include <ctime>      // 用于生成随机数相关
-
+#include <math.h>
+#include <vector>
+#include <cmath>
+#include <windows.h>
 using namespace std;
-
-// 游戏区域的宽度和高度
-const int WIDTH = 10;
-const int HEIGHT = 20;
-
-// 方块结构体，用于表示每个小方块的坐标
-struct Block {
-    int x;
-    int y;
-};
-
-// 各种形状的俄罗斯方块模板
-// 这里用二维数组来表示每个形状的相对坐标布局，例如：
-// 0 0 0 0
-// 0 1 1 0
-// 0 1 1 0
-// 0 0 0 0 代表一个田字方块，1表示方块所在位置
-// 下面定义了几种常见形状
-Block shapes[7][4] = {
-    { {0, 0}, {0, 1}, {1, 0}, {1, 1} },  // 田字方块
-    { {0, 0}, {0, 1}, {0, 2}, {0, 3} },  // 长条方块（竖）
-    { {0, 0}, {1, 0}, {2, 0}, {3, 0} },  // 长条方块（横）
-    { {0, 0}, {0, 1}, {0, 2}, {1, 2} },  // L形方块（一种）
-    { {0, 2}, {1, 0}, {1, 1}, {1, 2} },  // L形方块（另一种）
-    { {0, 0}, {1, 0}, {1, 1}, {1, 2} },  // 反Z形方块
-    { {0, 1}, {1, 0}, {1, 1}, {2, 0} }   // Z形方块
-};
-
-// 游戏区域，用于记录每个位置是否有方块存在，0表示空，1表示有方块
-int gameBoard[HEIGHT][WIDTH] = { 0 };
-
-// 当前方块的坐标和形状索引
-Block currentBlock[4];
-int currentShapeIndex;
-
-// 初始化游戏，比如设置初始方块等
-void initGame(){
-    srand(static_cast<unsigned int>(time(nullptr)));
-    currentShapeIndex = rand() % 7;
-    for (int i = 0; i < 4; ++i) {
-        currentBlock[i].x = WIDTH / 2 + shapes[currentShapeIndex][i].x;
-        currentBlock[i].y = shapes[currentShapeIndex][i].y;
-    }
+void xingxing();
+void bankrate();
+void random_month();
+void redbag();
+void redbag1();
+void array_test();
+int test(int a)
+{
+	static int n = a / 2;
+	a&& test(a - 1);
+	a < n ? cout << setw(n - a) << "*" << setw(2 * a + 2) << "*" << endl : a  > n + 1 ? cout << setw(a - n) << "*" << setw(2 * (2 * n - a + 1)) << "*" << endl : cout;
+	return  n;
 }
 
-// 在游戏区域绘制方块，根据传入的方块坐标数组和是否清空标记来绘制或清除方块
-void drawBlock(Block block[], bool clear = false) {
-    for (int i = 0; i < 4; ++i) {
-        int x = block[i].x;
-        int y = block[i].y;
-        if (x >= 0 && x < WIDTH && y >= 0 && y < HEIGHT) {
-            HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
-            if (clear) {
-                SetConsoleTextAttribute(hConsole, FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE);
-                gameBoard[y][x] = 0;
-            }
-            else {
-                SetConsoleTextAttribute(hConsole, FOREGROUND_INTENSITY | FOREGROUND_GREEN);
-                gameBoard[y][x] = 1;
-            }
-            COORD coord = { static_cast<SHORT>(x), static_cast<SHORT>(y) };
-            SetConsoleCursorPosition(hConsole, coord);
-            cout << (clear ? " " : "#");
-        }
-    }
+int main()
+{
+	//cout << "请输入半菱形高度n" << endl;
+	//int n = 0;
+	//cin >> n;
+	//cout << setw(n) << "*" << endl;
+	//test(2 * n - 2);
+	//cout << setw(n) << "*" << endl;
+	////system("pause");
+	//return 0;
+	//按键进行循环
+	array_test();
+	return 0;
+
+	//四个数字中最大的数字
+	/*int a[5], max;
+	cout << "请输入四个数字：";
+	cin >> a[1] >> a[2] >> a[3] >> a[4];
+	max = a[a[a[1] > a[2] ? 1 : 2] > a[a[3] > a[4] ? 3 : 4] ? a[1] > a[2] ? 1 : 2 : a[3] > a[4] ? 3 : 4];
+	cout << "最大值为：" << max << endl;
+	return 0;*/
 }
 
-// 检查当前方块是否可以移动到指定的偏移量位置
-bool canMove(int offsetX, int offsetY) {
-    for (int i = 0; i < 4; ++i) {
-        int newX = currentBlock[i].x + offsetX;
-        int newY = currentBlock[i].y + offsetY;
-        if (newX < 0 || newX >= WIDTH || newY >= HEIGHT || (newY >= 0 && gameBoard[newY][newX])) {
-            return false;
-        }
-    }
-    return true;
+//n个月后的利率问题
+
+
+void bankrate()
+{
+	double principal, rate, time, interest;
+	cout << "Enter the principal amount: ";
+	cin >> principal;
+	cout << "Enter the annual interest rate: ";
+	cin >> rate;
+	cout << "Enter the number of monthes: ";
+	cin >> time;
+	while (time>0)
+	{
+		principal += principal * rate / 100;
+		time--;
+	}
+	
+	cout << "The interest earned is " << principal << endl;
+	
 }
 
-// 旋转方块，尝试顺时针旋转当前方块
-void rotateBlock() {
-    Block rotatedBlock[4];
-    int pivotX = currentBlock[1].x;
-    int pivotY = currentBlock[1].y;
-    for (int i = 0; i < 4; ++i) {
-        int x = currentBlock[i].x - pivotX;
-        int y = currentBlock[i].y - pivotY;
-        rotatedBlock[i].x = pivotX - y;
-        rotatedBlock[i].y = pivotY + x;
-    }
-    if (canMove(0, 0)) {
-        drawBlock(currentBlock, true);
-        for (int i = 0; i < 4; ++i) {
-            currentBlock[i] = rotatedBlock[i];
-        }
-        drawBlock(currentBlock);
-    }
-}
+//编写一个随机产生1和12之间的整数的程序，并且根据数字显示相应的月份单词
 
-// 方块下落逻辑，让方块自动下落一格，如果不能下落则固定方块并生成新方块
-void dropBlock() {
-    if (canMove(0, 1)) {
-        drawBlock(currentBlock, true);
-        for (int i = 0; i < 4; ++i) {
-            currentBlock[i].y++;
-        }
-        drawBlock(currentBlock);
-    }
-    else {
-        drawBlock(currentBlock);
-        initGame();
-    }
+void random_month()
+{
+	int month;
+	month = rand()%12+1;
+	const char *month_name[] = { "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December" };
+	cout << "This month is " << month_name[month-1] << endl;
 }
+void xingxing()
+{
+	/*cout << "请输入半菱形高度n" << endl;
+	int n = 0, i = 1;
+	cin >> n;
+	cout << setw(n) << "*" << endl;
+	for (; i < n; ++i)
+		cout << setw(n - i) << "*" << setw(2 * i) << "*" << endl;
+	i = i - 2;
+	while (i > 0)
+	{
+		cout << setw(n - i) << "*" << setw(2 * i) << "*" << endl;
+		i--;
+	}*/
+	/*[capture] (parameters) mutable ->return-type{ statement }*/
+	//cout << setw(n) << "*" << "\t\t" << endl;
+	//lambda表达式
 
-// 处理用户输入，根据按键来移动或旋转方块等
-void handleInput() {
-    if (_kbhit()) {
-        char key = _getch();
-        switch (key) {
-        case 'a':
-            if (canMove(-1, 0)) {
-                drawBlock(currentBlock, true);
-                for (int i = 0; i < 4; ++i) {
-                    currentBlock[i].x--;
-                }
-                drawBlock(currentBlock);
-            }
-            break;
-        case 'd':
-            if (canMove(1, 0)) {
-                drawBlock(currentBlock, true);
-                for (int i = 0; i < 4; ++i) {
-                    currentBlock[i].x++;
-                }
-                drawBlock(currentBlock);
-            }
-            break;
-        case 's':
-            if (canMove(0, 1)) {
-                drawBlock(currentBlock, true);
-                for (int i = 0; i < 4; ++i) {
-                    currentBlock[i].y++;
-                }
-                drawBlock(currentBlock);
-            }
-            break;
-        case 'w':
-            rotateBlock();
-            break;
-        }
-    }
+	//auto testlambda = [](int a, int b) {return a *b; };
+	//auto testlambda2 = [](int a, int b)->double{return (a + b) / 2.0; };
+	//cout << testlambda(2, 3) << endl;
+	//cout << testlambda2(2, 3) << endl;
+	//int x = 10;
+	//int y = 20;
+	//auto printValues = [&x, &y]() { x+=10; y+=10; cout << "x=" << x << " y=" << y << endl; };
+	//printValues();
+
+	vector<int> numbers = { 1, 2, 3, 4, 5 };
+	numbers.push_back(6);
+	// 使用 lambda 表达式打印每个元素
+	for_each(numbers.begin(), numbers.end(), [](int num) { cout << num << " "; });
+	cout << endl; // 输出: 1 2 3 4 5
+	cout << "数字为: " << numbers.begin()[0] << endl; // 输出: 5
+	numbers.pop_back(); // 删除最后一个元素
+	// 使用 lambda 表达式对向量进行排序
+	sort(numbers.begin(), numbers.end(), [](int a, int b) { return a > b; });
+	cout << "数字为: "<< numbers.begin()[0]<< endl; // 输出: 5
+	// 打印排序后的向量
+	for (int num : numbers) {
+		cout << num << " ";
+	}
+	//找出tan(x)=1时x的值
+	int x = 0;
+	auto tanlam = [=]() { return tan(0.78539816); };
+	cout << "角度为45的正切值为: "<<tanlam() << endl;
+	cout << endl; // 输出: 5 4 3 2 1
+
 }
-//
-// 显示游戏区域
-void displayBoard() {
-    system("cls");  // 清屏
-    for (int y = 0; y < HEIGHT; ++y) {
-        for (int x = 0; x < WIDTH; ++x) {
-            cout << (gameBoard[y][x] ? "#" : " ");
-        }
-        cout << endl;
-    }
+void redbag1()
+{
+	int i, number;
+	int best;//手气最佳
+	float total;
+
+	cout << "请输入红包金额：";
+	cin >> total;
+	cout << "请输入抢红包人数：";
+	cin >> number;
+	/* 生成随机数 */
+	// 设置种子
+	srand((unsigned)time(NULL));
+	float a[1024];//保存每个人的随机数。最多支持1024个人抢红包。
+	float b[1024];//保存每个人获得的红包金额。
+	float suma = 0;//随机数总和。
+	float sumb = 0;//红包总和。
+	int max = 0;
+	for (i = 0; i < number; i++)
+	{
+		// 生成实际的随机数
+		a[i] = rand() % 100;
+
+		if (a[i] > max) {
+			max = a[i];
+			best = i;//获取手气最佳
+		}
+		suma += a[i];
+	}
+
+	for (i = 0; i < number - 1; i++)
+	{
+		b[i] = a[i] / suma * total;//按照随机数计算每个人实际获得的金额
+		sumb += round(b[i] * 100) / 100.0;//将红包金额保留两位小数
+		//输出信息
+		cout << "第" << setiosflags(ios::right) << setw(3) << i + 1 <<
+			"个人的红包是:" << setiosflags(ios::right) << setw(6) <<
+			setiosflags(ios::fixed) << setprecision(2) <<
+			round(b[i] * 100) / 100.0;
+		if (best == i) {
+			cout << "(手气最佳)" << endl;
+		}
+		else {
+
+			cout << endl;
+		}
+	}
+	//最后一人的红包金额等于总金额减去前面的金额。
+	cout << "第" << setiosflags(ios::right) <<
+		setw(3) << number << "个人的红包是:" <<
+		setiosflags(ios::right) << setw(6) << setiosflags(ios::fixed) <<
+		setprecision(2) << round((total - sumb) * 100) / 100.0;
+	if (best == number - 1) {
+		cout << "(手气最佳)" << endl;
+	}
+	else {
+
+		cout << endl;
+	}
 }
-//主函数
-int main() {
-    initGame();
-    drawBlock(currentBlock);
-    while (true) {
-        displayBoard();
-        handleInput();
-        dropBlock();
-        Sleep(500);
-    }
-    return 0;
+void redbag()
+{
+	int i, number, best;
+	float total;
+	std::cout << "请输入总额：" << std::endl;
+	std::cin >> total;
+	std::cout << "请输入个数：" << std::endl;
+	std::cin >> number;
+
+	// 配置种子
+	/*std::srand(std::time(0));*/
+	std::srand(42);
+	// 根据实际需要动态分配数组
+	float* rand_number = new float[number];
+	float* person_money = new float[number];
+	float sum_rand_number = 0;
+	float sum_person_money = 0;
+	int max = 0;
+
+	for (i = 0; i < number; i++) {
+		rand_number[i] = std::rand() % 100;
+
+		if (rand_number[i] > max) {
+			max = rand_number[i];
+			best = i;
+		}
+		sum_rand_number += rand_number[i];
+	}
+
+	for (i = 0; i < number - 1; i++) {
+		person_money[i] = rand_number[i] / sum_rand_number * total;
+		person_money[i] = std::round(person_money[i] * 100) / 100.0; // 保留两位小数
+		sum_person_money += person_money[i];
+
+		std::cout << "第" << i + 1 << "个金额为：" << person_money[i] << std::endl;
+	}
+	person_money[i] = total - sum_person_money;
+	std::cout << "第" << i + 1 << "个金额为：" << person_money[i] << std::endl;
+	std::cout << "最佳运气为:" << person_money[best] << std::endl;
+
+	// 释放动态分配的内存
+	delete[] rand_number;
+	delete[] person_money;
+
+	// 是否循环
+	std::cout << "是否继续？y/n" << std::endl;
+	char c;
+	std::cin.ignore(); // 清空输入缓冲区
+	std::cin >> c;
+	while (c == 'y' || c == 'Y') {
+		redbag();
+	}
+}
+void array_test()
+{
+	int arr[] = { 6,7,4,4,5 };
+	int random_arr[5];
+	for (int i = 0; i < 5; i++)
+	{
+		random_arr[i] = rand() % 100;
+	}
+	for (int i = 0; i < 5; i++)
+	{
+		std::cout << "第"<<i+1<<"个随机数为：" << setw(5) << random_arr[i] << std::endl;
+	}
 }
